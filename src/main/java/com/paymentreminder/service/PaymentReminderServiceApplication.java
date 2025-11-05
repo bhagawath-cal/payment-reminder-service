c38f513ae69e0962596307ed1ffa98c008c9353e
DeathBringer10041 <bkanta@umich.edu>
2025-11-05 09:19:44 -0600

    Initial commit

---
+++ src/main/java/com/paymentreminder/service/PaymentReminderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paymentreminder.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReminderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReminderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentreminder/service/PaymentReminderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentreminder.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReminderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
